fe32d6ee6a7b0e2d72a15eaafbc07538b001fa2a
Mr. Zerr <88155918+LysdexicGent@users.noreply.github.com>
2026-04-16 09:08:45 -0600

    Update hello_world_and_variables.py

--- Code Save/Intro to Python/hello_world_and_variables.py
+++ Code Save/Intro to Python/hello_world_and_variables.py
@@ -1,1 +1,31 @@
+#Mr. Zerr, 11/3, Intro to Python
+print("Hello World!")
 
+# Variable types
+variable_string = "This is a string"
+variable_int = 10
+varible_list = ["This","is","a","list"]
+
+print(variable_string)
+print(variable_int)
+print(varible_list)
+
+# working with variables
+input("Press Enter")
+print(variable_int + 5)
+
+count = 0
+input("Press Enter")
+while count < 10:
+    count += 1
+    print(count)
+    
+# IF/ELSE Statement
+input("Press Enter")
+
+if count < 10:
+    print("Count is greater than or equal to 10")
+elif count == 10:
+    print("Count is 10")
+else:
+    print("This is the Else.")
