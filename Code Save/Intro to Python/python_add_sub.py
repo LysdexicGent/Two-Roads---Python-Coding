1d4c7f6ad815e610507cf5cd0a835d7ad7c8db5f
Mr. Zerr <88155918+LysdexicGent@users.noreply.github.com>
2026-04-16 09:10:13 -0600

    Create python_add_sub.py

---
+++ Code Save/Intro to Python/python_add_sub.py
@@ -0,0 +1,39 @@
+# Mr. Zerr, 11/5, Simple Decisions
+"""
+START
+    PRINT "Adding or Subtracting"
+    PROMPT "DO you want + or -?"
+    INPUT selection
+    PROMPT "enter your two numbers"
+    PRINT "If - selected, second num is subtracted"
+    INPUT num1
+    INPUT num2
+    IF selection = +
+        num1 + num2 = total
+        PRINT total
+    ELSE
+        num1 - num2 = total
+        PRINT total
+    PRINT "END MESSAGE"
+END
+"""
+
+# main code
+num1 = 0
+num2 = 0
+total = 0
+selection = " "
+
+print("Adding or Subtracting machine. If Subtracting then 2nd number is subtracted from 1st number.")
+selection = input("Do you want + or -")
+num1 = int(input("Input 1st number: "))
+num2 = int(input("Input 2nd number: "))
+
+if selection == "+":
+    total = num1 + num2
+    print(total)
+else:
+    total = num1 - num2
+    print(total)
+    
+print("This is a exit message.")
